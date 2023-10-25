1816a4fe579e616b61cbea3394c15574a4118cb1
Dave <jdmh210202@gmail.com>
2023-10-25 12:11:51 -0600

    Implementacion con la base de datos

---
+++ Learn2SwitchApi-master/Domain.Endpoint/Services/UserServices.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Endpoint.DTOs;
+using Domain.Endpoint.Entities;
+using Domain.Endpoint.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Domain.Endpoint.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserServices(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;       
+        }
+        public User createUser(UserDTO newUserDTO)
+        {
+            
+
+                User nuevoUsuario = new User()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = newUserDTO.FirstName,
+                    LastName = newUserDTO.LastName,
+                    Email = newUserDTO.Email,
+                    Gender = newUserDTO.Gender,
+                    SchoolName = newUserDTO.SchoolName,
+                    GradeLevel = newUserDTO.GradeLevel,
+                    Password = newUserDTO.Password
+                    
+                };
+                _userRepository.Create(nuevoUsuario);
+                return nuevoUsuario;
+
+            
+
+
+
+            
+        }
+
+
+
+        public User FilterUser(Guid id)
+        {
+            return _userRepository.GetById(id);
+        }
+
+        public Task<List<User>> Get()
+        {
+            return _userRepository.GetAll();
+        }
+    }
+}
---
+++ Learn2SwitchApi-master/Learn2SwitchApi/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Domain.Endpoint;
+using Domain.Endpoint.DTOs;
+using Domain.Endpoint.Entities;
+using Domain.Endpoint.Services;
+using Microsoft.AspNetCore.Http;
+
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn2SwitchApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServices _userService;
+
+        public UserController(IUserServices userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public ActionResult<User> Create(UserDTO nuevoUser)
+        {
+            User newUser = _userService.createUser(nuevoUser);
+            return Ok(newUser);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUsuarioById(Guid Id)
+        {
+            User usuario = _userService.FilterUser(Id);
+            return Ok(usuario);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsuario()
+        {
+            List<User> usuario = await _userService.Get();
+            return Ok(usuario);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginDTO campos)
+        {
+            List<User> usuarios = await _userService.Get();
+            ServiceEncryptDecrypt enc = new ServiceEncryptDecrypt();
+
+            bool autenticado = false;
+
+            foreach (var user in usuarios)
+            {
+                string password = enc.Decrypt(user.Password);
+
+                
+                if (user.Email == campos.Email && password == campos.Password)
+                {
+                    autenticado = true;
+                    break; // Sal del bucle si la autenticación es exitosa.
+                }
+            }
+
+            if (autenticado)
+            {
+                return Ok("Login successful");
+            }
+            else
+            {
+                return Ok("Login denied");
+            }
+        }
+
+    }
+}
